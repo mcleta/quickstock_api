f08f75b1498340a66b28f04af52dfb7a86b67884
mucl <murillocasemiro@gmail.com>
2023-04-27 17:30:18 -0300

    config instance uuid

--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 // Retorna lista de pedidos do banco
 router.get('/', ( req, res, next ) => {
@@ -24,7 +25,9 @@ router.get('/num_order=:num_order', ( req, res, next ) => {
 
 // Insere um pedido no banco
 router.post('/', ( req, res, next ) => {
+    let myuuid = uuidv4();
     const order = {
+        num_order: myuuid,
         client: req.body.client,
         employee: req.body.employee,
         itens: req.body.itens,
