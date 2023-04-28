ec603d7067bb54f1e5fbea681fbf814b7ebd4fd4
mucl <murillocasemiro@gmail.com>
2023-04-28 00:08:29 -0300

    item crud implementation

--- routes/items.js
+++ routes/items.js
@@ -1,52 +1,121 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 // Retorna lista de itens no banco
 router.get('/', ( req, res, next ) => {
-    res.status(200).send({
-        mensage: 'ok orrrrrk'
+    mysql.getConnection(( error, connection ) => {
+        if ( error ) { return res.status( 500 ).send({ error: error }) };
+        connection.query(
+            'SELECT * FROM items',
+            ( error, result, field ) => {
+                connection.release();
+                if ( error ) { return res.status( 500 ).send({ error: error }) };
+                return res.status( 200 ).send({ response: result }); 
+            }
+        );
     });
 });
 
 // Retorna um produto com base no nome
 router.get('/name=:name_item', ( req, res, next ) => {
-    const name_item = req.params.name_item;
-    if (name_item != null) {
-        res.status(200).send({
-            mensage: 'ok orrrrrk'
-        });
-    } else {
-        res.status(404).send({
-            mensage: 'Item não encontrado'
-        });
-    }
+    let name_item = req.params.name_item;
+    mysql.getConnection(( error, connection ) => {
+        if ( error ) { return res.status( 500 ).send({ error: error }) };
+        connection.query(
+            'SELECT * FROM items WHERE name_item = ?',
+            [ name_item ],
+            ( error, result, field ) => {
+                connection.release();
+                if ( error ) { return res.status( 500 ).send({ error: error, message: "Name not found" }) };
+                return res.status( 200 ).send({ response: result }); 
+            }
+        );
+    });
 });
 
 // Insere um item no banco
 router.post('/', ( req, res, next ) => {
-    const item = {
+    let item = {
         name_item: req.body.name_item,
         quantity: req.body.quantity,
         location: req.body.location
     };
-
-    res.status(201).send({
-        mensage: 'item insert',
-        iItem: item
+    mysql.getConnection(( error, connection ) => {
+        if ( error ) {
+            return res.status( 500 ).send({
+                error: error
+            });
+        };
+        connection.query(
+            'INSERT INTO items (name_item, quantity, location) VALUES (?,?,?);',
+            [
+                item.name_item, 
+                item.quantity, 
+                item.location
+            ],
+            ( error, result, field ) => {
+                connection.release();
+                if ( error ) { return res.status( 500 ).send({ error: error, response: null }) };
+                res.status( 201 ).send({
+                    mensage: 'Item inserted successfully!!!',
+                    id_tem: result.InsertId
+                });
+            }
+        );
     });
 });
 
 // Atualiza um item no banco
 router.patch('/', ( req, res, next ) => {
-    res.status(200).send({
-        mensage: 'ok offffk'
+    let up_item = {
+        name_item: req.body.name_item,
+        quantity: req.body.quantity,
+        location: req.body.location,
+        id_item: req.body.id_item
+    };
+    mysql.getConnection(( error, connection ) => {
+        if ( error ) { return res.status( 500 ).send({ error: error }) };
+        connection.query(
+            `
+                UPDATE items 
+                    SET name_item   = ?, 
+                        quantity    = ?, 
+                        location    = ?
+                    WHERE id_item   = ?
+            `,
+            [
+                up_item.name_item, 
+                up_item.quantity, 
+                up_item.location,
+                up_item.id_item
+            ],
+            (error, result, field) => {
+                connection.release();
+                if ( error ) { return res.status( 500 ).send({ error: error, response: null }) };
+                return res.status( 202 ).send({
+                    mensage: 'Successfully changed item!!!',
+                    id_tem: result.InsertId
+                });
+            }
+        );
     });
 });
 
 // Deleta um item no banco
 router.delete('/', ( req, res, next ) => {
-    res.status(204).send({
-        mensage: 'Item excluido com sucesso'
+    let id_exclude_item = req.body.id_item;
+    mysql.getConnection(( error, connection ) => {
+        if ( error ) { return res.status( 500 ).send({ error: error }) };
+        connection.query(
+            'DELETE FROM items WHERE id_item = ?',
+            [ id_exclude_item ],
+            ( error, result, field ) => {
+                connection.release();
+                if ( error ) { return res.status( 500 ).send({ error: error, message: "Item not found" }) };
+                return res.status( 202 ).send({ message: "Successfully deleted item!!!"}); 
+            }
+        );
     });
 });
 
